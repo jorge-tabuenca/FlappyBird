90f0dc21e8392df9ed5e63d1404e0cbc51436a65
jorge-tabuenca <jorgetabuenca99@gmail.com>
2021-01-22 15:28:45 +0100

    Initial commit

---
+++ desktop/src/com/flappy/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.flappy.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.flappy.game.FlappyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyGame(), config);
+	}
+}
