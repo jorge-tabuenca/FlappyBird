8e047c37c17fec67377ca0b2f727b61ffb239b72
jorge-tabuenca <jorgetabuenca99@gmail.com>
2021-01-22 16:45:07 +0100

    fourth commit - bird can't fall under y = 0

--- core/src/com/flappy/game/states/PlayState.java
+++ core/src/com/flappy/game/states/PlayState.java
@@ -1,5 +1,6 @@
 package com.flappy.game.states;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.flappy.game.FlappyGame;
@@ -8,16 +9,20 @@ import com.flappy.game.sprites.Bird;
 public class PlayState extends State{
 
     private Bird bird;
+    private Texture bg;
 
     protected PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50, 300);
         cam.setToOrtho(false, FlappyGame.WIDTH / 2, FlappyGame.HEIGHT / 2);
+        bg = new Texture("bg.png");
     }
 
     @Override
     public void handleInput() {
-
+        if(Gdx.input.isTouched()) {
+                bird.jump();
+        }
     }
 
     @Override
@@ -30,6 +35,7 @@ public class PlayState extends State{
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
+        sb.draw(bg, cam.position.x - (cam.viewportWidth / 2), 0);
         sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
         sb.end();
     }
