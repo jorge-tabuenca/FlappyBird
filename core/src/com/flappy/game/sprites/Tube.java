13803dfc43997e296c150ed38c25aafeff20003d
jorge-tabuenca <jorgetabuenca99@gmail.com>
2021-01-22 16:52:42 +0100

    fifth commit - tube aleatory appear at the scree, now i need to make that in a loop

---
+++ core/src/com/flappy/game/sprites/Tube.java
@@ -0,0 +1,41 @@
+package com.flappy.game.sprites;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.Random;
+
+public class Tube {
+
+    private static final int FLUCTUATION = 130;
+    private static final int TUBE_GAP = 100; //opening between tubes
+    private static final int LOWEST_OPENING = 120;
+
+    private Texture topTube, bottomTube;
+    private Vector2 posTopTube, posBottomTube;
+    private Random rand;
+
+    public Tube(float x){
+        topTube = new Texture("toptube.png");
+        bottomTube = new Texture("bottomtube.png");
+        rand = new Random();
+        posTopTube = new Vector2(x, rand.nextInt(FLUCTUATION) + LOWEST_OPENING + TUBE_GAP);
+        posBottomTube = new Vector2(x, posTopTube.y - TUBE_GAP - bottomTube.getHeight());
+    }
+
+    public Texture getTopTube() {
+        return topTube;
+    }
+
+    public Texture getBottomTube() {
+        return bottomTube;
+    }
+
+    public Vector2 getPosTopTube() {
+        return posTopTube;
+    }
+
+    public Vector2 getPosBottomTube() {
+        return posBottomTube;
+    }
+}
