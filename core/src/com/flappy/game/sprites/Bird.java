b10f45e8198f526b1556f1bc2f8b2a058dcd9ff8
jorge-tabuenca <jorgetabuenca99@gmail.com>
2021-01-25 18:35:10 +0100

    eith commit - bird animation

--- core/src/com/flappy/game/sprites/Bird.java
+++ core/src/com/flappy/game/sprites/Bird.java
@@ -1,6 +1,7 @@
 package com.flappy.game.sprites;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 
@@ -11,6 +12,7 @@ public class Bird {
     private Rectangle bounds;
     private Vector3 position;
     private Vector3 velocity;
+    private Animation birdAnimation;
 
     private Texture bird;
 
@@ -18,10 +20,13 @@ public class Bird {
         position = new Vector3(x, y, 0);
         velocity = new Vector3(0, 0, 0);
         bird = new Texture("bird.png");
-        bounds = new Rectangle(x, y, bird.getWidth()/3, bird.getHeight());
+        Texture texture = new Texture("birdanimation.png");
+        birdAnimation = new Animation(new TextureRegion(texture), 3, 0.5f);
+        bounds = new Rectangle(x, y, texture.getWidth() / 3, texture.getHeight());
     }
 
     public void update(float dt){
+        birdAnimation.update(dt);
         if(position.y > 0)
             velocity.add(0, GRAVITY, 0);
         velocity.add(0, GRAVITY, 0);
@@ -43,8 +48,8 @@ public class Bird {
         return position;
     }
 
-    public Texture getTexture() {
-        return bird;
+    public TextureRegion getTexture() {
+        return birdAnimation.getFrame();
     }
 
     public Rectangle getBounds(){
